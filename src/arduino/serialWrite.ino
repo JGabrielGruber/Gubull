296665150752d5ea6359fb59655929670cdb53fb
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-08-30 21:29:23 -0300

    Implementado retorno de peso;

--- src/arduino/serialWrite.ino
+++ src/arduino/serialWrite.ino
@@ -15,11 +15,16 @@ void loop() {
       Serial.print("@@");
       hello = true;
       delay(100);
-    } else if (hello && request == "37") {
+  } else if (hello && request == "36") {
       delay(100);
       Serial.print("$");
-      Serial.print(12345);
+      Serial.print(12345); // O BRINCO
       Serial.print("$");
+  } else if (hello && request == "37") {
+      delay(100);
+      Serial.print("%");
+      Serial.print(54.34); // O PESO
+      Serial.print("%");
     } else if (request == "33"){
       hello = false;
       Serial.print("#");
